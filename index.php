d2a9a84c63aaf49b4c5a5aeb2ab55943922cefb4
youssef_el_mofid <ymofid18@gmail.com>
2023-12-28 20:54:36 +0100

    add button delele

--- index.php
+++ index.php
@@ -5,6 +5,17 @@ try {
     $sql->execute();
     $taches  = $sql->fetchAll();
     extract($_POST);
+    if (isset($action)) {
+        if ($action == "new") {
+            $sql = $db->prepare("INSERT INTO todo(title,done)VALUES (?,0)");
+            $sql->execute([$tache]);
+            header("location:./index.php");
+        }elseif ($action == "delete") {
+            $sql = $db->prepare("DELETE FROM todo WHERE id=?");
+            $sql->execute([$id]);
+            header("location:./index.php");
+        }
+    }
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
