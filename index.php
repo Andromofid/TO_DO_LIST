bb28d0c376bffe1b3e362572ff83889d74aca7c5
youssef_el_mofid <ymofid18@gmail.com>
2023-12-28 21:22:26 +0100

    link with bootstrap

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO("mysql:host=localhost;dbname=todolist","root","");
+    $db = new PDO("mysql:host=localhost;dbname=todolist", "root", "");
     $sql = $db->prepare("SELECT * FROM todo");
     $sql->execute();
     $taches  = $sql->fetchAll();
@@ -10,11 +10,11 @@ try {
             $sql = $db->prepare("INSERT INTO todo(title,done)VALUES (?,0)");
             $sql->execute([$tache]);
             header("location:./index.php");
-        }elseif ($action == "delete") {
+        } elseif ($action == "delete") {
             $sql = $db->prepare("DELETE FROM todo WHERE id=?");
             $sql->execute([$id]);
             header("location:./index.php");
-        }else {
+        } else {
             $sql = $db->prepare("UPDATE todo SET done = 1-done WHERE id = ?");
             $sql->execute([$id]);
             header("location:./index.php");
@@ -26,13 +26,17 @@ try {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="bootstrap.min.css">
-    <script src="bootstrap.min.css"></script>
+    <!-- <link rel="stylesheet" href="bootstrap.min.css">
+    <script src="bootstrap.min.css"></script> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <nav class="nav bg-dark px-4">
         <h1 class="text-white">ToDoList</h1>
@@ -73,4 +77,5 @@ try {
         </ul>
     </div>
 </body>
+
 </html>
